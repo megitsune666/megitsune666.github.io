c0e6c26ed0763b3dc67dbc08da3edd61f63f5880
megitsune666 <agiszon666@gmail.com>
2026-01-31 16:12:47 +0100

    fix cover

--- js/fnc_data_v2.js
+++ js/fnc_data_v2.js
@@ -96,7 +96,7 @@ var ary_CharacterData = [
   [1, "ICONIC",                   [0,0,0,0,0,1,0,0,0,0], "Sorter/HOTTOPIC.jpg"],
   [1, "Spicy Queen",                   [0,0,0,0,0,1,0,0,0,0], "Sorter/HOTTOPIC.jpg"],
   [1, "トキメキAbout you",                   [0,0,0,0,0,1,0,0,0,0], "Sorter/HOTTOPIC.jpg"],
-  [1, "GIRL'S TALK",               [0,0,0,0,0,1,0,0,0,0,0], "Sorter/BT.jpg"],
+  [1, "GIRL'S TALK",               [0,0,0,0,0,1,0,0,0,0,0], "Sorter/HOTTOPIC.jpg"],
   [1, "はなびえんちゃん。のテーマ (Hanabiechan's Theme)",          [0,0,0,0,0,1,0,0,0,0], "Sorter/HOTTOPIC.jpg"]
 
 
